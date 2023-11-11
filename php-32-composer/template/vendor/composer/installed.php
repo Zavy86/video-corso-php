18d459d939fdc1ec86bbc1dc79b928ca57a9db6d
Manuel Zavatta <manuel.zavatta@gmail.com>
2023-11-11 18:09:12 +0100

    add 32 composer

---
+++ php-32-composer/template/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'zavy86/video-corso-php',
+        'pretty_version' => '0.0.1',
+        'version' => '0.0.1.0',
+        'reference' => NULL,
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'zavy86/video-corso-php' => array(
+            'pretty_version' => '0.0.1',
+            'version' => '0.0.1.0',
+            'reference' => NULL,
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
